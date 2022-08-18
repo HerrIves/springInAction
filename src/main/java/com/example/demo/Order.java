2b9a2cb5d1d8934c2df9521b9fe69f74dbd253bc
HerrIves <spechtator@gmail.com>
2022-08-18 15:51:49 +0200

    p48 2.3.2

--- src/main/java/com/example/demo/Order.java
+++ src/main/java/com/example/demo/Order.java
@@ -1,14 +1,33 @@
 package com.example.demo;
 
 import lombok.Data;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
 @Data
 public class Order {
+    @NotBlank(message = "name is requaired")
     private String name;
+    @NotBlank(message = "street is requaired")
     private String street;
+    @NotBlank(message = "city is requaired")
     private String city;
+    @NotBlank(message = "state is required")
     private String state;
+    @NotBlank(message = "zip code is required")
     private String zip;
+
+    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
+
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",message="Must be formatted MM/YY")
     private String ccExpiration;
+
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
+
 }
--- src/main/java/com/example/demo/Taco.java
+++ src/main/java/com/example/demo/Taco.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -10,6 +11,7 @@ import lombok.Data;
 public class Taco {
 
     @NotNull
+    @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
     private List<String> ingredients;
 }
