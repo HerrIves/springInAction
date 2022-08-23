832a744a164a301a2ee5d52fc1bb2ee5c6020c8b
HerrIves <spechtator@gmail.com>
2022-08-23 17:07:16 +0200

    small ref pom.xml

--- src/main/java/com/example/demo/TacoCloudApplication.java
+++ src/main/java/com/example/demo/TacoCloudApplication.java
@@ -6,19 +6,13 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TacoCloudApplication implements WebMvcConfigurer {
+public class TacoCloudApplication /*implements WebMvcConfigurer*/{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
 /*
-	By extending an existing configuration class, you can avoid creating a new configuration
-	class, keeping your project artifact co unt down. But I tend to prefer creating a
-	new configuration class for each kind of configuration (web, data, security, and so
-	on), keeping the application bootstrap configuration clean and simple.
-
-
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("home");
