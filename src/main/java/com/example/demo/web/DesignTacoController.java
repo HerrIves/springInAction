4d1caa2ee674a7deeb35c7fd0a2ab2f01a35325b
HerrIves <spechtator@gmail.com>
2022-08-26 15:57:42 +0200

    p 74 Order dont work

--- src/main/java/com/example/demo/web/DesignTacoController.java
+++ src/main/java/com/example/demo/web/DesignTacoController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.example.demo.Order;
 import com.example.demo.Taco;
 import com.example.demo.data.IngredientRepository;
 import com.example.demo.data.TacoRepository;
@@ -28,8 +29,17 @@ import javax.validation.Valid;
 @SessionAttributes("order")
 public class DesignTacoController {
 
-    private final IngredientRepository ingredientRepo;
+    @ModelAttribute(name = "order")
+    public Order order() {
+        return new Order();
+    }
 
+    @ModelAttribute(name = "tako")
+    public Taco taco() {
+        return new Taco();
+    }
+
+    private final IngredientRepository ingredientRepo;
     private TacoRepository designRepo;
 
     @Autowired
@@ -42,19 +52,7 @@ public class DesignTacoController {
 
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient>ingredients = new ArrayList<>();
-/*                Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
- */
+        List<Ingredient> ingredients = new ArrayList<>();
         ingredientRepo.findAll().forEach(ingredient -> ingredients.add(ingredient));
 
         Type[] types = Ingredient.Type.values();
@@ -68,16 +66,20 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(/*@Valid*/ @ModelAttribute("design") Taco design, Errors errors){
+    public String processDesign(
+            /*@Valid не работает*/ /*@ModelAttribute("design")в книге нету*/ Taco design,
+                                                                             Errors errors,
+                                                             @ModelAttribute Order order) {
+        if (errors.hasErrors()) { return "design"; }
 
-        if (errors.hasErrors())
-            return "design";
+        Taco saved = designRepo.save(design);
+        order.addDesign(saved);
 
         log.info("Process design: " + design);
         return "redirect:/orders/current";
     }
 
-    private List<Ingredient> filterByType(List<Ingredient>ingredients, Type type) {
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
         return ingredients
                 .stream()
                 .filter(x -> x.getType().equals(type))
